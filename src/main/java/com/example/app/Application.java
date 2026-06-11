bca0a5a28484c073f061865a884d2be419a5a5c9
somayaj <somayajulaas@gmail.com>
2026-06-10 20:44:09 -0500

    checkin for spretta

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
         SpringApplication.run(Application.class, args);
     }
 }
